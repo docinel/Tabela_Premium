992fdf6c8e949f3aad1408d3e3389f25e90f11cf
docinel <docinel@gmail.com>
2023-10-10 01:20:07 -0300

    Novos precos e incluido id

--- tabela.py
+++ tabela.py
@@ -8,42 +8,41 @@ st.set_page_config(
 )
 
 st.title(':bed: TABELA DE PREÇOS PREMIUM COLCHOES :cloud:')
-st.title(':cloud:  :sunglasses: :moneybag:')
 
 
 def formatar(valor):
     return "{:,.f}".format(valor)
 
+
 df = pd.read_excel(
     io='tabela_premium.xlsx',
     engine='openpyxl',
-    usecols=range(0, 3)
-    
+    usecols=range(0, 4)
+
 )
 
-codigo=st.sidebar.multiselect(
+codigo = st.sidebar.multiselect(
   "SELECIONE OU DIGITE OS CODIGOS:",
-   options=df["CODIGOS"].unique(),
-   #default=df["CODIGOS"].unique()  
+  options=df["CODIGOS"].unique(),
   )
 
-# produto=st.sidebar.multiselect(
+# produto = st.sidebar.multiselect(
 #   "SELECIONE OS PRODUTOS:",
-#    options=df["PRODUTOS"].unique(),
-#    #default=df["CODIGOS"].unique()  
+#   options=df["PRODUTOS"].unique(),
 #   )
 
 df_select = df.query(
     "CODIGOS == @codigo",
 )
+
 st.markdown("---")
 total_produtos = df_select["VALORES"].sum()
 texto_soma_total_produtos = f"{total_produtos: _.2f}"
-texto_soma_total_produtos = texto_soma_total_produtos.replace('.', ',').replace('_', '.')
+texto_soma_total_produtos = texto_soma_total_produtos.replace('.', ',').\
+                                                        replace('_', '.')
 left_column, middle_column, right_column = st.columns(3)
 with right_column:
     st.write(f"TOTAL DA SELEÇÃO R$ {texto_soma_total_produtos}")
 
 
 st.table(df_select)
-
